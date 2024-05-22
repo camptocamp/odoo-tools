ad9f855a057ac4891de41e27d1a737dafd744cc9
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-05-22 17:06:22 +0200

    Creation of 17.0 branch

--- camptocamp_migration_tools/__manifest__.py
+++ camptocamp_migration_tools/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Camptocamp Migration tools",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp",
     "license": "AGPL-3",
     "category": "Others",
