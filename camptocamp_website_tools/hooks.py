2fa47d07f8828132f0c2b37d070a7523e9b8dc3a
ilo <dev@nomadiplus.com>
2024-05-24 12:20:50 -0300

    C2C-1721: Adding readme on some modules

--- camptocamp_website_tools/hooks.py
+++ camptocamp_website_tools/hooks.py
@@ -2,19 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-def post_init_hook(env, registry):
+def post_init_hook(cr, registry):
     query = """
         UPDATE ir_ui_view
         SET customize_show = False, active = False
         WHERE key = 'website.show_website_info';
     """
-    env.cr.execute(query)
+    cr.execute(query)
 
 
-def uninstall_hook(env, registry):
+def uninstall_hook(cr, registry):
     query = """
         UPDATE ir_ui_view
         SET customize_show = True
         WHERE key = 'website.show_website_info';
     """
-    env.cr.execute(query)
+    cr.execute(query)
