e6b9b1f15ed3bf41b3ceadae995727dc4047198d
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-05-27 15:19:35 +0200

    [FIX] CI / precommit
    update pylint config for odoo 17
    replace use of setuptools with whool

--- camptocamp_migration_tools/__manifest__.py
+++ camptocamp_migration_tools/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-{
+
+{  # noqa: B018
     "name": "Camptocamp Migration tools",
     "version": "17.0.1.0.0",
     "author": "Camptocamp",
