bcb60c6c065a2d0145d6c3b79f6fc9bb55cdbb3f
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-10-16 09:24:28 +0200

    [MIG] camptocamp_website_tools: Migration to 18.0

--- camptocamp_website_tools/__manifest__.py
+++ camptocamp_website_tools/__manifest__.py
@@ -3,7 +3,7 @@
 
 {  # noqa: B018
     "name": "Camptocamp Website",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp",
     "license": "AGPL-3",
     "category": "Others",
